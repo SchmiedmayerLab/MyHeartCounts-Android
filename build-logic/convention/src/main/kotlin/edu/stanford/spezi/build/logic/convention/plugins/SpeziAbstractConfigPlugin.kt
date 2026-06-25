738d882376df4debe64a5ee14d1bbf78e8e7ba42
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2026-06-25 00:54:35 -0700

    Initial Commit

---
+++ build-logic/convention/src/main/kotlin/edu/stanford/spezi/build/logic/convention/plugins/SpeziAbstractConfigPlugin.kt
@@ -0,0 +1,33 @@
+package edu.stanford.spezi.build.logic.convention.plugins
+
+import edu.stanford.spezi.build.logic.convention.extensions.apply
+import edu.stanford.spezi.build.logic.convention.extensions.findBundle
+import edu.stanford.spezi.build.logic.convention.extensions.implementation
+import edu.stanford.spezi.build.logic.convention.extensions.testImplementation
+import edu.stanford.spezi.build.logic.convention.model.PluginId
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.provideDelegate
+
+abstract class SpeziAbstractConfigPlugin(private val modulePlugin: PluginId) : Plugin<Project> {
+    private val defaultConfig by lazy { SpeziBaseConfigConventionPlugin() }
+
+    override fun apply(project: Project) = with(project) {
+        apply(modulePlugin)
+        apply(PluginId.JETBRAINS_KOTLIN_ANDROID)
+
+        defaultConfig.apply(this)
+
+        dependencies {
+            if (path != LOGGING_MODULE) {
+                implementation(project(LOGGING_MODULE))
+            }
+            testImplementation(findBundle("unit-testing"))
+        }
+    }
+
+    private companion object {
+        const val LOGGING_MODULE = ":core-logging"
+    }
+}
